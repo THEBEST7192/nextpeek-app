e8cca65f3b2059d16d87fe291e7b4591df8c8fa2
thebest7192 <123459429+THEBEST7192@users.noreply.github.com>
2025-11-04 11:02:31 +0100

    feat(build): Add application logo and configure electron forge
    Add logo assets in multiple formats (ico, png, icns) and update forge config to use them as application icons

--- forge.config.js
+++ forge.config.js
@@ -4,12 +4,15 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: 'src/assets/logo/logo',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: './src/assets/logo/logo.ico'
+      },
     },
     {
       name: '@electron-forge/maker-zip',
